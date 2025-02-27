65b6d23b5535349d1f3a2db97a9c65b2109511e4
Neo Onyedire <156123182+Naylormenace@users.noreply.github.com>
2025-02-27 12:45:24 +0200

    Add files via upload

--- cash register/cash_register.py
+++ cash register/cash_register.py
@@ -1,24 +1,16 @@
 def change(ammount_paid, ammount_due):
-  denominations = ["200","100","50","20","10","5","2","1"]
-  change_due = int(round(ammount_paid - ammount_due, 2) *100)
-  change_dict = {}
+    denominations = [20000, 10000, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5 ]
+    change_due = ammount_paid - ammount_due
+    change_dict = {}
 
-  for i in denominations:
-    count = change_due // int(i)
-    if count > 0:
-      change_dict[f"R{int(i) / 100}"] = count  # Store the count with denomination
-      change_due -= count * int(i) # Subtract the value of the denomination from the change_due
-  return change_dict
-      
-ammount_1 = 12250  # R250
+    for i in denominations:
+        count = change_due // i
+        if count > 0:
+            change_dict[f"R{i:.2f}"] = int(count) # Format the denomination correctly and store the count
+            change_due -= count * i # Subtract the value of the denomination from the change_due
+    return change_dict
+
+ammount_1 = 250  # R250
 ammount_2 = 183.75  # R183.75
 change_1 = change(ammount_1, ammount_2)
 print(change_1)
-      
-
-  # for a,b in zip(ammount_paid, ammount_due):
-  #   if ammount_due > ammount_paid:
-  #     return ValueError
-  #   if ammount_due == ammount_paid:
-  #     return "no change"
-  #   if ammount_due < ammount_paid:
--- cash register/test_cash_register.py
+++ cash register/test_cash_register.py
@@ -3,6 +3,6 @@ from cash_register import *
 
 
 class test_Change(unittest.TestCase):
-    def test_min_lent(self): 
+    def test_change(self): 
         self.assertTrue(change("R50.50"))
         self.assertFalse(change("-3"))
